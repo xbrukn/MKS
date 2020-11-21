45e44378a110f563b08db15e2bab25cf3b2f23d7
xbrukn <xbrukn00@vutbr.cz>
2020-11-21 22:17:22 +0100

    CV04_1

---
+++ CV04/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: 8. 11. 2020
+ *      Author: User
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led(uint32_t value);
+void sct_init(void);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
