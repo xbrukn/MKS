d1c412d24253c3fc232e819d31e019d7c5cc800b
xbrukn <xbrukn00@vutbr.cz>
2020-12-06 21:44:01 +0100

    CV05_03

--- CV05/Core/Src/main.c
+++ CV05/Core/Src/main.c
@@ -25,6 +25,7 @@
 
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 
 /* USER CODE END Includes */
 
@@ -43,10 +44,14 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_rx;
 
 /* USER CODE BEGIN PV */
+
+#define EEPROM_ADDR 0xA0
 
 #define CMD_BUFFER_LEN 64
 
@@ -63,6 +68,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 int _write(int file, char const *buf, int n)
@@ -94,33 +100,58 @@ static void uart_process_command(char *cmd)
 		 printf("OK\n");
 	}
 	else if (strcasecmp(token, "LED2") == 0)
-		{
-			token = strtok(NULL, " ");
-			if (strcasecmp(token, "ON") == 0)
-				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+	{
+		token = strtok(NULL, " ");
+		if (strcasecmp(token, "ON") == 0)
+			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
 
-			 else if (strcasecmp(token, "OFF") == 0)
-					 HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
-			 printf("OK\n");
-		}
+		 else if (strcasecmp(token, "OFF") == 0)
+				 HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+		 printf("OK\n");
+	}
 	else if (strcasecmp(token, "STATUS") == 0)
-			{
-			/*
-			int LED1_status = HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin);
-			int LED2_status = HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin);
+	{
+		token = strtok(NULL, " ");
+		printf("Stav: ");
 
-			printf("LED1: " , LED1_status ,"\n");
-			printf("LED2: " , LED2_status ,"\n");
-			*/
-			printf("Stav: ");
+		if(HAL_GPIO_ReadPin(LED1_GPIO_Port, LED1_Pin) == 0) printf("LED1 OFF ");
+		else printf("LED1 ON ");
 
-			if(HAL_GPIO_ReadPin(LED1_GPIO_Port, LED1_Pin) == 0) printf("LED1 OFF ");
-			else printf("LED1 ON ");
+		if(HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin) == 0) printf("LED2 OFF \n");
+		else printf("LED2 ON \n");
+	}
+	else if (strcasecmp(token, "READ") == 0)
+	{
+		token = strtok(NULL, " ");
+		uint16_t addr = atoi(token);
+		uint8_t value;
 
-			if(HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin) == 0) printf("LED2 OFF \n");
-			else printf("LED2 ON \n");
+		HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
 
-			}
+		printf("Adresa 0x%04X = 0x%02X\n",addr,value);
+
+		printf("OK\n");
+	}
+	else if (strcasecmp(token, "WRITE") == 0)
+	{
+		token = strtok(NULL, " ");
+		uint16_t addr = atoi(token);
+		uint8_t value;
+
+		HAL_I2C_Mem_Write(&hi2c1, EEPROM_ADDR, addr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
+
+		while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT)
+		{
+
+		}
+
+		printf("OK\n");
+	}
+	else if (strcasecmp(token, "DUMP") == 0)
+	{
+		token = strtok(NULL, " ");
+
+	}
 	else
 	{
 		printf("Chyba\n");
@@ -178,6 +209,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_UART_Receive_DMA(&huart2, uart_rx_buf, RX_BUFFER_LEN);
@@ -222,6 +254,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -249,6 +282,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
